bd994226ecd0e8723f80df8aced800c74d9da7a8
minhhien <hienminh.ce@gmail.com>
2025-09-04 13:57:18 +0700

    Exercise 2

---
+++ Core/Inc/led_7seg.h
@@ -0,0 +1,16 @@
+/*
+ * led_7seg.h
+ *
+ *  Created on: Sep 4, 2025
+ *      Author: qwein
+ */
+
+#ifndef INC_LED_7SEG_H_
+#define INC_LED_7SEG_H_
+
+#include "main.h"
+
+void display7SEG(int num);
+
+
+#endif /* INC_LED_7SEG_H_ */
